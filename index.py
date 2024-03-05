48f390ded044f0c4a5659dc315fb57d6d45074ca
Tiaguh <tiagosingup@gmail.com>
2024-03-04 23:03:51 -0300

    improving code

--- index.py
+++ index.py
@@ -18,7 +18,19 @@ palavras = [
     "desafio"
 ]
 
-palavra_sorteada = random.randrange(len(palavras))
-palavra_sorteada = palavras[palavra_sorteada]
+palavra_sorteada = random.choice(palavras)
+palavra_a_ser_adivinhada = ['_' for i in palavra_sorteada ]
 
-palpite = str(input("Digite uma letra: "))
+while '_' in palavra_a_ser_adivinhada: 
+    palpite = input("Digite uma letra: ").lower()  
+
+    if palpite in palavra_sorteada:
+        for i in range(len(palavra_sorteada)):
+            if palavra_sorteada[i] == palpite:
+                palavra_a_ser_adivinhada[i] = palpite
+    else:
+        print("Letra incorreta. Tente novamente.")
+
+    print("Palavra: " + ' '.join(palavra_a_ser_adivinhada))
+
+print("Parabéns! Você adivinhou a palavra:", palavra_sorteada)
