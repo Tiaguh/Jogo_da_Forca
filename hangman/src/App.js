ed899c5b7435072c9c18463fe914bd644f5dceb8
Tiaguh <tiagosingup@gmail.com>
2024-03-06 16:13:10 -0300

    consuming the api dicionario aberto

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -1,6 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export default function App() {
+  const [word, setWord] = useState('');
+  const [guess, setGuess] = useState('');
+
+  useEffect(() => {
+    const fetchWord = async () => {
+      try {
+        const response = await axios.get('https://api.dicionario-aberto.net/random');
+        setWord(response.data.word.toLowerCase());
+      } catch (error) {
+        console.error('Erro ao buscar as palavras:', error);
+      }
+    };
+
+    fetchWord();
+  }, []);
+
   return (
-    <>
-    </>
+    <div>
+      <h1>Hangman Game</h1>
+      {word}
+
+      <input onChange={(e)=> setGuess(e.target.value)}  />
+
+      {guess}
+
+    </div>
   );
-}
+};
